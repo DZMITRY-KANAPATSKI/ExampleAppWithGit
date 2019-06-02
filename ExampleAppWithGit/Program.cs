e5f3a26039084a7e5f8510beaa724d1a0c910a69
Дмитрий Конопацкий <mega.konopatskiy@mail.ru>
2019-06-02 15:13:14 +0300

    meeting

--- ExampleAppWithGit/Program.cs
+++ ExampleAppWithGit/Program.cs
@@ -10,6 +10,8 @@ namespace ExampleAppWithGit
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
